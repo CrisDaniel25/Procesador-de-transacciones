dfb2ca11075dbb2ecb2f9edf28bd9bbec3974462
CrisDaniel25 <63526691+CrisDaniel25@users.noreply.github.com>
2020-11-12 14:18:52 -0400

    [UPDATE]

--- Index.php
+++ Index.php
@@ -22,14 +22,14 @@
 <div class="container">
     <div class="row">
 
-        <table class="table table-hover">
+        <table class="table table-hover"> 
 	        <thead class="thead-dark">
              <th>Id</th>
              <th>Fecha - Hora</th>
              <th>Monto</th>
              <th>Descripción</th>
              <th>
-             <a class="btn btn-success btn-lg btn-block" href="formularios/agregar/Create.php">
+             <a class="btn btn-success btn-lg btn-block btn-table" href="formularios/agregar/Create.php">
               <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-plus-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
               </svg>
@@ -45,14 +45,14 @@
                 <td>
                 <div class="row">
                 <div class="col-sm">
-                 <a href="Delete.php?id=<?php echo $key;?>" class="btn btn-danger btn-lg btn-block">
+                 <a href="metodos/eliminar/Delete.php" class="btn btn-danger btn-lg btn-block btn-table">
                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                      <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
                     </svg>
                  </a>
                   </div>
                   <div class="col-sm">
-                 <a href="formularios/editar/Edit.php" class="btn btn-warning btn-lg btn-block">
+                 <a href="formularios/editar/Edit.php" class="btn btn-warning btn-lg btn-block btn-table">
                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                       <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                       <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
