77a41c03b1ccb5d696c4b7bc9e7fc65b4e9f1ab3
CrisDaniel25 <63526691+CrisDaniel25@users.noreply.github.com>
2020-11-11 20:42:07 -0400

    Add Project

---
+++ Index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarea - Procesador de transacciones</title>
+</head>
+<body>
+    <table>
+        <thead>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+</body>
+</html>
