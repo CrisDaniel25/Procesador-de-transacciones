e47f46df9ca6d4a792b060e27be5cbe40e4b65b0
CrisDaniel25 <63526691+CrisDaniel25@users.noreply.github.com>
2020-11-12 20:39:09 -0400

    [UPDATE]

--- metodos/agregar/Add.php
+++ metodos/agregar/Add.php
@@ -1,3 +1,16 @@
 <?php
-header('location:../../Index.php');
+require_once '../../transaciones/transaciones.php';
+
+    $date;
+    $transancion = new transaciones();
+
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        date_default_timezone_set("America/Santo_Domingo");
+        $date = date("Y-m-d h:i:sa");
+        $transancion.InitData(0,$date,$_POST['monto'],$_POST['descripcion']);
+        echo $date;
+
+
+    }
+    // header('location:../../Index.php');
 ?>
